ff17f4067f0b2e19e519e4fdf1b3eee0318cf4fd
Jialin Liu <valiantljk@gmail.com>
2018-02-18 19:39:06 +0000

    io code parser

---
+++ test/extract_io_call.py
@@ -0,0 +1,6 @@
+vorpal_io = /build/benchmarks/trunk/benchmarks/write/vorpalio/
+hacc_io = /build/benchmarks/hacc-io/
+vpic_io = /build/benchmarks/vpic-io/
+h5boss_io = /build/benchmarks/h5boss-dev/
+
+
