b055028f057d3ab9374b8019b7e700d2427d1ddb
Jialin Liu <valiantljk@gmail.com>
2018-02-24 14:09:17 -0800

    refactor codes; create pythom_vol package

---
+++ src/python_vol/__init__.py
@@ -0,0 +1,16 @@
+try:
+    from h5pvol import H5PVol
+except Exception as e:
+    print ("Python HDF5 Vol is not implemented")
+
+try:
+    from h5svol import H5PVol
+except Exception as e:
+    print ("Python Swift Vol is not implemented")
+    pass
+
+def h5py():
+    return H5PVol
+
+def swift():
+    return H5PVol
