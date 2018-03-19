545f2f76ed2e045ceea157f2e6767713e7ec61c0
Jialin Liu <valiantljk@gmail.com>
2018-03-19 14:21:15 -0700

    reformat swift.py

--- src/swift.py
+++ src/swift.py
@@ -14,27 +14,22 @@ logging.getLogger("swiftclient").setLevel(logging.CRITICAL)
 logger = logging.getLogger(__name__)
 
 def swift_list(container):
- container = container
- minimum_size = -0.1
- with SwiftService() as swift:
-    try:
-        list_parts_gen = swift.list(container=container)
-	print (list_parts_gen)
-        for page in list_parts_gen:
-            if page["success"]:
-                for item in page["listing"]:
-
-                    i_size = int(item["bytes"])
-                    if i_size > minimum_size:
-                        i_name = item["name"]
-                        i_etag = item["hash"]
-                        print(
-                            "%s [size: %s] [etag: %s]" %
-                            (i_name, i_size, i_etag)
-                        )
-            else:
-		print page
-                raise page["error"]
-
-    except SwiftError as e:
-        logger.error(e.value)
+	container = container
+	minimum_size = -0.1
+	with SwiftService() as swift:
+		try:
+			list_parts_gen = swift.list(container=container)
+			print (list_parts_gen)
+			for page in list_parts_gen:
+				if page["success"]:
+					for item in page["listing"]:
+						i_size = int(item["bytes"])
+						if i_size > minimum_size:
+							i_name = item["name"]
+							i_etag = item["hash"]
+							print("%s [size: %s] [etag: %s]" %(i_name, i_size, i_etag))
+				else:
+					print (page)
+					raise (page["error"])
+		except SwiftError as e:
+			logger.error(e.value)
