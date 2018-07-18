17b6aec81be6e8101e178a5634cdf4c71262ad7b
valiantljk <valiantljk@gmail.com>
2018-07-18 02:43:31 -0700

    start dset parallel write

--- test/parallel_swift.sh
+++ test/parallel_swift.sh
@@ -4,9 +4,12 @@ echo 'start'
 LD_LIBRARY_PATH_BEFORE=$LD_LIBRARY_PATH
 echo 'source final config to add python path into lib'
 source final.config.nersc
-
-srun -n 2 ./HDF5_file_create_parallel some.h5 2 # run as ./HDF5_file_create_parallel filename number_of_files
-srun -n 2 ./HDF5_objects_create_parallel some.h5 mygroup mydset 3 10 20 30 # run as ./HDF5_objects_create_parallel filename groupname datasetname ndims dims0 dims1
+#echo 'file create'
+#srun -n 2 ./HDF5_file_create_parallel liu1.h5 2 # run as ./HDF5_file_create_parallel filename number_of_files
+#echo 'objects create'
+#srun -n 2 ./HDF5_objects_create_parallel liu2.h5 mygroup mydset 3 10 20 30 # run as ./HDF5_objects_create_parallel filename groupname datasetname ndims dims0 dims1
+echo 'dataset write'
+srun -n 2 ./HDF5_dataset_write_parallel liu_xss3.h5 mygroup mydset 3 10 20 30 # run as ./HDF5_objects_create_parallel filename groupname datasetname ndims dims0 dims1
 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH_BEFORE
 echo 'done'
