ca7eb6ed701c1702b3a9d1ca01a426fe78d5cb64
valiantljk <valiantljk@gmail.com>
2018-08-16 00:50:41 -0700

    update in bdcats

--- test/misc/extract_io_call.py
+++
@@ -1,6 +0,0 @@
-vorpal_io = /build/benchmarks/trunk/benchmarks/write/vorpalio/
-hacc_io = /build/benchmarks/hacc-io/
-vpic_io = /build/benchmarks/vpic-io/
-h5boss_io = /build/benchmarks/h5boss-dev/
-
-
